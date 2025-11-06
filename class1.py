cd38c9defc1cb51aa64d6089dda287cd40118198
akatapore80 <akatapore80@gmail.com>
2025-11-06 12:08:12 +0200

    first commit

---
+++ class1.py
@@ -0,0 +1,97 @@
+def my_fisrFunction(num1,num2,num3):
+    return num1 + num2 + num3
+
+
+print(my_fisrFunction(2,1,3))
+print(my_fisrFunction(10,12,31))
+
+def my_F2(n2,v,k):
+    return n2 -v + k
+print(my_F2(20,9,6))
+myVariable = 'Hello world!' 
+print(myVariable)
+
+# Global variable 
+Saf = 2 
+def myFunction():
+     b = 3
+print(myFunction())
+print(Saf)
+
+a = "Hello"
+b = "World"
+d = 'bye'
+c =  a + " " + b + ' ' + d
+print(c)
+# Example 1: Joining two words
+word1 = "Good"
+word2 = "Morning"
+print(word1 + " " + word2)
+
+# Example 2: Joining three words
+greeting = "How"
+middle = "are"
+end = "you?"
+print(greeting + " " + middle + " " + end)
+
+# Example 3: Creating a sentence
+name = "Alice"
+verb = "is"
+activity = "studying"
+print(name + " " + verb + " " + activity + ".")
+
+# Example 4: Joining words to form a phrase
+color = "Blue"
+object = "Sky"
+print(color + " " + object)
+
+# Example 5: Combining more than three words
+part1 = "Python"
+part2 = "is"
+part3 = "fun"
+part4 = "to"
+part5 = "learn!"
+print(part1 + " " + part2 + " " + part3 + " " + part4 + " " + part5)
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+txt = "The best things in life are free!"
+print("free" in txt)
+print("in" in txt)
+print("is" in txt)
+
+
+# Example 1
+a = "Dear"
+b = "Sir"
+c = a + " " + b
+print(c) 
+
+# Example 2
+x = "Birds"
+y = "can fly"
+z = x + " " + y
+print(z) 
+
+# Example 3
+first = "The"
+second = "sun"
+third = "is bright"
+sentence = first + " " + second + " " + third
+print(sentence) 
+
+# Example 4
+word1 = "She"
+word2 = "runs"
+combined = word1 + " " + word2
+print(combined) 
+
+# Example 5
+part1 = "I"
+part2 = "saw"
+part3 = "a shooting star"
+message = part1 + " " + part2 + " " + part3
+print(message)
