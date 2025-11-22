fa4e8ca4abb1bc62b7b97bb03d0cdb006f83ff32
akatapore80 <akatapore80@gmail.com>
2025-11-22 11:53:21 +0200

    sum

---
+++ Teacher/mySum.py
@@ -0,0 +1,8 @@
+
+# Odd or even sum: Take two numbers, add them, and check if the sum is odd or even.
+def mySum(a,b):
+ if (a + b) % 2 ==0:
+   print("Even number")
+ else:
+   print("Odd number")
+mySum(4,11)
