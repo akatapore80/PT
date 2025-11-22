b3ac239b26e77ef690cdbe51431b05bb285ae85b
obafemi david <obafemidavprince@gmail.com>
2025-11-22 11:20:31 +0100

    login system

---
+++ Teacher/nestedCondition.py
@@ -0,0 +1,9 @@
+# Nested condition example: Input a number and check if it is positive; if positive, further check if it is greater than 100.
+
+def nestedFunction(num) :
+    if num > 0: 
+       if num > 100 : 
+          print("it is greater than 100")
+
+nestedFunction(120)
+nestedFunction(40)
