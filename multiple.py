2b73e49b34a079662ae25e55b7e1cf464db5b391
akatapore80 <akatapore80@gmail.com>
2025-11-27 10:59:10 +0200

    moret

---
+++ multiple.py
@@ -0,0 +1,18 @@
+
+# Check Multiples
+
+# Ask the user to input two numbers.
+
+# Use a conditional to check if the first number is a multiple of the second.
+
+# Print "Yes" if true, otherwise "No
+num1 = input("Enter first number")
+num2 = input("Enter second number")
+num1 = int(input("Enter first number"))
+num2 = int(input("Enter second number"))
+def myMultiple(num1,num2):
+    if num1 % num2 == 0:
+        print("Yes")
+    else:
+        print("No")
+myMultiple(num1,num2)
