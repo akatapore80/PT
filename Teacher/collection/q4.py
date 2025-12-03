b017b6595a8491bb669ea8c4f164c51c8c50b0c7
obafemi david <obafemidavprince@gmail.com>
2025-12-03 11:48:03 +0100

    reverse list

---
+++ Teacher/collection/q4.py
@@ -0,0 +1,21 @@
+# Find the Largest Number in a List
+
+# Create a list of numbers.
+
+# Use a for loop to find the largest number without using max().
+
+# Print the largest number.
+
+
+listNumber = [2, 3, 1, 5, 6, 10, 4, 1]
+max = listNumber[0]
+for num in listNumber :
+
+    if num > max : 
+        max = num
+
+print(max)
+
+
+
+
---
+++ Teacher/collection/q5.py
@@ -0,0 +1,38 @@
+# Reverse a List Using a For Loop
+
+# Create a list of numbers: [1, 2, 3, 4, 5].
+
+# Use a for loop to reverse the list manually (without reverse() or slicing).
+
+# Print the reversed list.
+
+listNumber = [1, 2, 3, 4, 5]
+
+reverseList = []
+
+[5, 4, 3, 2, 1]
+
+for num in listNumber :
+
+    reverseList.insert(0, num)
+
+print(reverseList)
+
+# ----------------------------------------------------------------------- 2 solution
+reverseListRange = []
+
+for i in range(len(listNumber) - 1, -1, -1) :
+    reverseListRange.append(listNumber[i])
+
+print(reverseListRange)
+
+
+# Range(start, stop, interval)
+# len(listNumber) return 5
+# Range(len(listNumber) - 1, -1, -1)
+#        listNumber[4] 
+#  4-1 = listNumber[3]
+#  3-1 = listNumber[2]
+#  2-1 = listNumber[1]
+#  1-1 = listNumber[0]
+ 
