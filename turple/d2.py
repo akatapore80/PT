218ab152c840f5c2aaa34e425173b5091815a8f0
obafemi david <obafemidavprince@gmail.com>
2025-12-18 11:44:28 +0100

    d2

--- turple/d2.py
+++ turple/d2.py
@@ -1,13 +1,33 @@
 
 # 1. Given t = ((1,2,3), (4,5,6), (7,8,9)), write code to access 8.
-t = ((1,2,3), (4,5,6), (7,8,9))
+# 2 dimentional tuple
+
+t = (
+    (1,2,3), 
+    (4,5,6), 
+    (7,8,9)
+    )
+
 print(t[2][1])
 
 # 2. Concatenate two tuples and sort the resulting tuple.
+
+# first method
+
 tuple1 = (3, 1, 4)
 tuple2 = (2, 5, 6)
-tuple = tuple1 + tuple2 
-print(tuple)
+joinTuple = tuple1 + tuple2 
+convertToList = sorted(joinTuple)
+backToTuple = tuple(convertToList)
+print(backToTuple)
+
+# second method
+a = (5, 2, 9, 1, 5, 6)
+convertAToList = list(a)
+convertAToList.sort()
+convertBackToTuple = tuple(convertAToList)
+print(convertBackToTuple)
+
 
 # 3. Pack multiple values into a tuple, then unpack them into variables.
 packed = (10, 20, 30)
@@ -21,7 +41,6 @@ temp_list = list(t)
 temp_list[1] = 13
 print(temp_list)
 modified = tuple(temp_list)
-
 print(modified)  
 
 
