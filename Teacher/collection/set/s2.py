98dd95d4061eab88f48861479475fc28ad88b888
obafemi david <obafemidavprince@gmail.com>
2025-12-20 15:31:11 +0100

    set

--- Teacher/collection/set/s2.py
+++ Teacher/collection/set/s2.py
@@ -3,17 +3,17 @@
 
 def findPair(k) : 
     listNumber = [25, 10, 30, 20, 40, 30, 50]
-    savePair = []
+    savePair = set()
     for i in range(len(listNumber)) : 
-        for index in range(len(listNumber)) :
-            if i == index :
+        for x in range(len(listNumber)) :
+            if i == x :
                 continue
             else : 
-                sumItem = listNumber[i] + listNumber[index]
+                sumItem = listNumber[i] + listNumber[x]
                 if sumItem == k :
-                    savePair.append(listNumber[i])
-                    savePair.append(listNumber[index])
-    return set(savePair)
+                    newList = (listNumber[i], listNumber[x])
+                    savePair.add(tuple(sorted(newList)))
+    return savePair
 
 
 print(findPair(50))
